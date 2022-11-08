b7ebe74c4a4d03c7032431fa269eb6f6743f738c
CCreech <ccreech@airway.com>
2022-11-08 12:15:26 -0500

    Update icon_carrier png and svg

--- Skrapper/Skrapper.Android/Resources/Resource.designer.cs
+++ Skrapper/Skrapper.Android/Resources/Resource.designer.cs
@@ -22030,16 +22030,19 @@ namespace Skrapper.Droid
 			public const int splash_screen = 2131165340;
 			
 			// aapt resource value: 0x7F07009D
-			public const int test_custom_background = 2131165341;
+			public const int telegram = 2131165341;
 			
 			// aapt resource value: 0x7F07009E
-			public const int tooltip_frame_dark = 2131165342;
+			public const int test_custom_background = 2131165342;
 			
 			// aapt resource value: 0x7F07009F
-			public const int tooltip_frame_light = 2131165343;
+			public const int tooltip_frame_dark = 2131165343;
 			
 			// aapt resource value: 0x7F0700A0
-			public const int xamarin_logo = 2131165344;
+			public const int tooltip_frame_light = 2131165344;
+			
+			// aapt resource value: 0x7F0700A1
+			public const int xamarin_logo = 2131165345;
 			
 			static Drawable()
 			{
